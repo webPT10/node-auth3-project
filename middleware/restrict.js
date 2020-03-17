e5e0de0e5d7d4d82cc97e2e8bcc6573a9a99d53c
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-17 16:18:35 -0700

    restrict.js >> setup

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,16 +1,30 @@
-const bcrypt = require("bcryptjs")
-const Users = require("../users/usersModel")
+const bcrypt = require("bcryptjs");
+const Users = require("../users/usersModel");
 
-function restrict(){
-
-    return async (req, res, next) => {
-        try {
-
-        } catch(error){
-            next(error)
-        }
-    }
-}
-module.exports = {
-    restrict
+function restrict() {
+  const authError = {
+    message: "Yo! Invalid Credentials"
   };
+  return async (req, res, next) => {
+    try {
+      const { username, password } = req.headers;
+      if (!username || !password) {
+        return res.status(401).json(authError);
+      }
+
+      const user = await Users.findBy({ username }).first();
+      if (!user) {
+        return res.status(401).json(authError);
+      }
+
+      const passwordValid = await bcrypt.compare(password, user.password);
+      if (!passwordValid) {
+        return res.status(401).json(authError);
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+module.exports = restrict;
