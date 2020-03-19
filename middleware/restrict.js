c427a51b4a90cb00a964a14e5d4032a04546d90a
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-19 10:23:10 -0700

    usersRouter > updated .post('login')

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcryptjs");
-const Users = require("../users/usersModel");
-const jwt = require("jsonwebtoken")
+// const bcrypt = require("bcryptjs");
+// const Users = require("../users/usersModel");
+const jwt = require("jsonwebtoken");
 
 function restrict() {
   const authError = {
@@ -9,23 +9,25 @@ function restrict() {
 
   return async (req, res, next) => {
     try {
-      const { token }  = req.cookies
-        console.log(req.cookies)
+      let token = req.headers.authorization;
 
-      if(!token){
-          return res.status(401).json(authError) 
+      if (!token) {
+        return res.status(401).json(authError);
       }
+      token = token.split(" ")[1]
+      console.log(token)
+
       // verify the token's signature
       jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-          if(error){
-              return res.status(401).json(authError)
-          }
+        if (error) {
+          return res.status(401).json(authError);
+        }
 
-          req.token = decoded
-          console.log(decoded)
+        req.token = decoded;
+        console.log(decoded);
 
-          next()
-      })
+        next();
+      });
     } catch (error) {
       next(error);
     }
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -3,56 +3,73 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const Users = require("./usersModel");
 const restrict = require("../middleware/restrict");
-const router = express.Router({
-    mergeParams: true
-  });
+const jwt = require("jsonwebtoken");
 
-// POST	/api/register	
-// Creates a user using the information sent inside the body of the request. 
+const router = express.Router({
+  mergeParams: true
+});
+
+// POST	/api/register
+// Creates a user using the information sent inside the body of the request.
 // Hash the password before saving the user to the database.
 //-------------------
 router.post("/register", async (req, res, next) => {
-    try{
-        const saved = await Users.add(req.body)
-        res.status(201).json(saved)
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+    const saved = await Users.add(req.body);
+    res.status(201).json(saved);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// POST	/api/login	
-// Use the credentials sent inside the body to authenticate the user. 
-// On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. 
+// POST	/api/login
+// Use the credentials sent inside the body to authenticate the user.
+// On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id.
 // If login fails, respond with the correct status code and the message: 'You shall not pass!'
 //-------------------
 router.post("/login", async (req, res, next) => {
-    try {
-      const { username, password } = req.body;
-      const user = await Users.findBy({ username }).first();
-      const passwordValid = await bcrypt.compare(password, user.password);
-      if (user && passwordValid) {
-        res.status(200).json({
-          message: `Welcome, ${username}!`
-        });
-      } else {
-        res.status(401).json({
-          message: "Invalid credentials - please try again"
-        });
-      }
-    } catch (error) {
-      next(error);
+    const authError = {
+        message: "Invalid Credentials"
     }
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first()
+    if(!user){
+        return res.status(401).json(authError)
+    }
+    const passwordValid = await bcrypt.compare(password, user.password);
+    if(!passwordValid){
+        return res.status(401).json(authError)
+    }
 
-// GET	/api/users	
-// If the user is logged in, respond with an array of all the users contained in the database. 
+    const payload = {
+        userId: user.id,
+        userRole: "user"
+    }
+    
+      // generate new JWT & cryptographically sign
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
+    //   console.log(token);
+
+    res.cookie("token", token)
+    res.json({
+        message: `Welcome, ${user.username}`
+    }) 
+   
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET	/api/users
+// If the user is logged in, respond with an array of all the users contained in the database.
 // If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
 router.get("/users", restrict(), async (req, res, next) => {
-    try {
-      const users = await Users.find();
-      res.json(users);
-    } catch (error) {
-      next(error);
-    }
-  });
-module.exports = router
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
